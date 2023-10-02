f571154aa9345d2c5d6591aa3a9f38e8653ebe79
ricardoggz <ricardo_ggzicm@outlook.com>
2023-10-02 16:03:34 -0600

    [add]: validacion de inputs de formulario

---
+++ scripts/checkedInput.js
@@ -0,0 +1,24 @@
+let radioInstitution = document.querySelectorAll('input[type="radio"][name="pertenece_institucion_publica"]');
+let radioProfession = document.querySelectorAll('input[type="radio"][name="es_dermatologo"]');
+let inputDisabledInstitution = document.getElementById('inputDisabledInstitution');
+let inputDisabledProfession = document.getElementById('inputDisabledProfession');
+
+for (var i = 0; i < radioInstitution.length; i++) {
+    radioInstitution[i].addEventListener('change', function() {
+    if (this.value === 'si') {
+        inputDisabledInstitution.style.display = 'block';
+    } else {
+        inputDisabledInstitution.style.display = 'none';
+    }
+});
+}
+
+for (var i = 0; i < radioProfession.length; i++) {
+    radioProfession[i].addEventListener('change', function() {
+    if (this.value === 'si') {
+        inputDisabledProfession.style.display = 'block';
+    } else {
+        inputDisabledProfession.style.display = 'none';
+    }
+});
+}
